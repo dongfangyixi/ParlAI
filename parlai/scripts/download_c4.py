48aea20cfd21754e21c1f8b77d5adabcf1458a92
lixu13 <lixu13@baidu.com>
2021-06-17 20:29:27 +0800

    sync

--- parlai/scripts/download_c4.py
+++ parlai/scripts/download_c4.py
@@ -5,6 +5,8 @@ get wet data path first
 """
 from tqdm import tqdm
 import wget
+import multiprocessing
+from multiprocessing import Pool
 
 def get_addr(local_addr):
     """
@@ -26,9 +28,12 @@ def download(path_file):
     urls = []
     with open(path_file, 'r') as f:
         for line in f:
-            urls.append(get_addr(line.strip()))
-    for url in tqdm(urls):
-        wget(url)
+    #         urls.append(get_addr(line.strip()))
+    # for url in tqdm(urls):
+    #     wget.download(url)
+
+    with Pool(10) as p:
+        p.map(wget.download, urls)
 
 
 if __name__ == "__main__":
